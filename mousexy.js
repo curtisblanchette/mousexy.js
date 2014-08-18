e7c1ea972eaed2c03d577a460fc48de8a79ac59b
curtisblanchette <curtis.blanchette88@gmail.com>
2014-08-17 17:32:38 -0700

    first commit of mousexy

---
+++ mousexy.js
@@ -0,0 +1,43 @@
+(function($){
+    $.fn.mousexy = function(options) {
+    
+    $(function () {
+        $(document).tooltip({
+            track: true
+        });
+    });
+
+    var $stage = $('#mousexy');
+
+
+    $stage.mousemove(function (e) {
+
+        var stageOffset = $stage.offset(),
+            relativeX = (e.pageX - stageOffset.left),
+            relativeY = (e.pageY - stageOffset.top); //minus the stages offset to achieve exact mousePos inside of div
+
+   /*     TweenMax.to($bubblesFront, 2, {
+            x: ((relativeX - $bubblesFW) / 16)
+        }); //set movement ammount by division
+        TweenMax.to($bubblesBack, 2, {
+            x: ((relativeX - $bubblesBW) / 32)
+        });
+        TweenMax.to($bubblesFront, 2, {
+            y: ((relativeY - $bubblesFH) / 16)
+        });
+        TweenMax.to($bubblesBack, 2, {
+            y: ((relativeY - $bubblesBH) / 32)
+        });
+
+        TweenMax.to($wave, 2, {
+            x: ((relativeX - $waveW) / 4)
+        });
+*/
+
+        $stage.html('x=' + relativeX + 'y=' + relativeY );
+    });
+
+
+
+    }//end plugin fn
+}(jQuery));
